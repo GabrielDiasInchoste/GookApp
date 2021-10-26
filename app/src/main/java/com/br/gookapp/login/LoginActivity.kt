7bc5e25b78726da089ef6ff2d7fd0f0dbb28613a
Gabriel Dias <gabriel.dias97@hotmail.com>
2021-10-26 20:03:16 -0300

    Initial Commit

---
+++ app/src/main/java/com/br/gookapp/login/LoginActivity.kt
@@ -0,0 +1,85 @@
+package com.br.gookapp.login
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.br.gookapp.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var gso: GoogleSignInOptions
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    val RC_SIGN_IN: Int = 1
+    lateinit var signOut: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val signIn = findViewById<View>(R.id.login_button) as SignInButton
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        signOut = findViewById<View>(R.id.logout_button) as Button
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        signIn.setOnClickListener { view: View? ->
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleResult(task)
+        } else {
+            Toast.makeText(this, "Problem in execution order :(", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun handleResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account: GoogleSignInAccount = completedTask.getResult(ApiException::class.java)
+            updateUI(account)
+        } catch (e: ApiException) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val dispTxt = findViewById<View>(R.id.emailTextView) as TextView
+        dispTxt.text = account.displayName
+        signOut.visibility = View.VISIBLE
+        signOut.setOnClickListener { view: View? ->
+            mGoogleSignInClient.signOut().addOnCompleteListener { task: Task<Void> ->
+                if (task.isSuccessful) {
+                    dispTxt.text = " "
+                    signOut.visibility = View.INVISIBLE
+                    signOut.isClickable = false
+                }
+            }
+        }
+    }
+}
